543833ced1ca9f2f8662dd561b9d22a0d112611c
comp20final <76014464+comp20final@users.noreply.github.com>
2020-12-14 22:53:23 -0500

    Update little-chef-start.js

--- little-chef-start.js
+++ little-chef-start.js
@@ -4,9 +4,10 @@ var fs = require('fs');
 
 http.createServer(function (req,res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	fs.readFile('./login.html', null, function (err, data){
+	res.write("test");
+	/*fs.readFile('./login.html', null, function (err, data){
 		if (err) throw err;
 		res.write(data);
-	});
+	});*/
 	res.end(); 
 }).listen(port);
